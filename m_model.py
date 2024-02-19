7b4f1e63836aabc1bc2e27f98a99a977fb1424af
swamisharan kumawat <147104986+Swamisharan1@users.noreply.github.com>
2024-02-20 01:26:36 +0530

    Update m_model.py

--- m_model.py
+++ m_model.py
@@ -65,7 +65,7 @@ def main():
     if st.button("Predict"):
     # Make prediction
         prediction = predict_price(input_data)
-        prediction_in_original_scale = prediction[0] * 100000
+        prediction_in_original_scale = prediction[0] * 1000000
         st.success(f"The estimated price of the house is ${prediction_in_original_scale:,.2f}")
         
 if __name__ == "__main__":
